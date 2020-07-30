58cfee30b42e046605c1b29161dc8b79f6a94d52
expert99 <rrricardo00@yahoo.com.br>
2020-07-30 15:45:34 -0300

    aula

--- src/components/layouts/card.jsx
+++ src/components/layouts/card.jsx
@@ -4,8 +4,9 @@ import React from 'react';
 export default props => {
 
     return (
+        
         <div className="card">
-            <h2>Card</h2>
+            
             <div className="title">{props.titulo}</div>
             <div className="content">conteúdo</div>
         </div>
