e1b67e916295998f1593e1c51ed40f05029e546f
expert99 <rrricardo00@yahoo.com.br>
2020-08-18 01:55:26 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,16 @@ export default () =>
     <div id="app">
         <h1>Fundamentos React</h1>
         <PrimeiroComponente></PrimeiroComponente>
-<<<<<<< HEAD
-        {/* <ComParametro titulo="titulo segundo componente" subtitulo="subtitulo segundo componente" nota={6}></ComParametro> */}
-        {/* <ComParametro titulo="titulo segundo componente 2" subtitulo="subtitulo segundo componente 2" nota={8}></ComParametro> */}
-        {/* <Fragmento></Fragmento> */}
-        {/* <Aleatorio  max={100} min={1}></Aleatorio> */}
-        <Card titulo="Componente com filhos"><Familia sobrenome="Rosa" terceiroNome="Robervaldo"></Familia></Card>
-        <Card titulo="Com parametro 1"><ComParametro titulo="titulo segundo componente" subtitulo="subtitulo segundo componente" nota={6}></ComParametro></Card>
-        <Card titulo="Com parametro 2"><ComParametro titulo="titulo segundo componente 2" subtitulo="subtitulo segundo componente 2" nota={8}></ComParametro></Card>
-        <Card titulo="Desafio aleatorio"><Aleatorio max={100} min={1}></Aleatorio></Card>
-        <Card titulo="Fragmento"><Fragmento></Fragmento></Card>
-=======
         <div className="cards" >
             {/* <ComParametro titulo="titulo segundo componente" subtitulo="subtitulo segundo componente" nota={6}></ComParametro> */}
             {/* <ComParametro titulo="titulo segundo componente 2" subtitulo="subtitulo segundo componente 2" nota={8}></ComParametro> */}
             {/* <Fragmento></Fragmento> */}
             {/* <Aleatorio  max={100} min={1}></Aleatorio> */}
+            <Card><Familia sobrenome="Rosa" terceiroNome="Silvas"></Familia></Card>
             <Card titulo="Com parametro 1" color="#080"><ComParametro titulo="titulo segundo componente" subtitulo="subtitulo segundo componente" nota={6}></ComParametro></Card>
             <Card titulo="Com parametro 2" color="#512"><ComParametro titulo="titulo segundo componente 2" subtitulo="subtitulo segundo componente 2" nota={8}></ComParametro></Card>
             <Card titulo="Desafio aleatorio" color="#035"><Aleatorio max={100} min={1}></Aleatorio></Card>
             <Card titulo="Fragmento" color="#509"><Fragmento></Fragmento></Card>
         </div>
->>>>>>> 38b7adb5aa5ccadf61f338e99877b3b6bf758b06
     </div>
 
